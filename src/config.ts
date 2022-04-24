31985f2cb8158a8980d69961105af6f34d2561c9
Jaden Zhang <jadenzhang@Jadens-MacBook-Pro.local>
2022-04-24 15:22:47 +0800

    Correct deployment configs

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ const config = () => ({
   NODE_ENV: process.env.NODE_ENV || 'dev',
   DATABASE_URL: process.env.DATABASE_URL || 'sqlite:///test.sqlite',
   EXPIRE_HOURS: process.env.EXPIRE_HOURS || 6,
-  ACALA_SUBQL_ENDPOINT: process.env.ACALA_SUBQL_ENDPOINT || 'wss://acala-parachain-wss.parallel.fi',
-  MOONBEAM_SUBQL_ENDPOINT: process.env.MOONBEAM_SUBQL_ENDPOINT || ''
+  ACALA_SUBQL_ENDPOINT: process.env.ACALA_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/acala-distribution-subql__cGFyY',
+  MOONBEAM_SUBQL_ENDPOINT: process.env.MOONBEAM_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/moonbeam-distribution-subql__cGFyY'
 });
 
 export default config;
