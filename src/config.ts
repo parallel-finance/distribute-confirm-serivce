c34dc38f772a52951f901f7330100a5afc478e8e
Jaden Zhang <jadenzhang@Jadens-MacBook-Pro.local>
2022-05-05 18:18:17 +0800

    Update config with staging subql endpoints

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ const config = () => ({
   NODE_ENV: process.env.NODE_ENV || 'dev',
   DATABASE_URL: process.env.DATABASE_URL || 'sqlite:///test.sqlite',
   EXPIRE_HOURS: process.env.EXPIRE_HOURS || 6,
-  ACALA_SUBQL_ENDPOINT: process.env.ACALA_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/acala-distribution-subql__cGFyY',
-  MOONBEAM_SUBQL_ENDPOINT: process.env.MOONBEAM_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/moonbeam-distribution-subql__cGFyY'
+  ACALA_SUBQL_ENDPOINT: process.env.ACALA_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/acala-distribute-subql-staging',
+  MOONBEAM_SUBQL_ENDPOINT: process.env.MOONBEAM_SUBQL_ENDPOINT || 'https://api.subquery.network/sq/parallel-finance/moonbeam-distribute-subql-staging'
 });
 
 export default config;
